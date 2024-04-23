861c81cee3ae0b0db35b72ebefdb60309f9ea443
lachlanriley <lril0001@student.monash.edu>
2024-04-23 11:26:56 +1000

    Added comments to vector-space model implentation

--- information-retrieval/tf-idf_cosine-similarity.ts
+++ information-retrieval/tf-idf_cosine-similarity.ts
@@ -2,22 +2,49 @@ class Vector {
 
     components: number[];
 
-    constructor(num_rows: number) {
-        this.components = new Array(num_rows).fill(0);
+    /**
+     * Initialises the vector as a list of 0's of a defined size
+     * 
+     * @param num_values - the size of the vector
+     */
+    constructor(num_values: number) {
+        this.components = new Array(num_values).fill(0);
     }
 
+    /**
+     * Returns the element from a given index
+     * 
+     * @param index - the index of the item being retrieved
+     * @returns the value at that index
+     */
     get_component(index: number): number {
         return this.components[index];
     }
 
+    /**
+     * Returns the size of the vector
+     * 
+     * @returns the length of the components array
+     */
     get_length(): number {
         return this.components.length;
     }
 
+    /**
+     * Modifies the value of a given index
+     * 
+     * @param index - the index of the value being modified
+     * @param value - the value being assigned to the particular index
+     */
     set_component(index: number, value: number): void {
         this.components[index] = value;
     }
 
+    /**
+     * Returns the Euclidean Length of the vector
+     * 
+     * @returns the square root of the sum of the squared components
+     */
     euclidean_length(): number {
         let run_sum = 0;
         this.components.forEach( (component) => {
@@ -26,6 +53,11 @@ class Vector {
         return run_sum ** 0.5;
     }
 
+    /**
+     * Increases the size of the vector and sets the respective values to 0
+     * 
+     * @param new_size - the number of elements the vector should contain 
+     */
     add_zeros(new_size: number): void {
         let current_length = this.get_length();
         let num_new_zeros = new_size - current_length;
@@ -34,7 +66,16 @@ class Vector {
 
 }
 
+
+/**
+ * Calculates the term-frequency vector of a given document and maintains a running record of the word frequencies
+ * 
+ * @param d - the document
+ * @param words_dict - dictionary keeping track of word frequencies
+ * @returns the tf vector and the updated dictionary of words which have appeared   
+ */
 function tf(d: string, words_dict: Record<string, number> = {}): [Vector, Record<string, number>] {
+
  
     let words: string[];
 
@@ -61,7 +102,14 @@ function tf(d: string, words_dict: Record<string, number> = {}): [Vector, Record
     
 }
 
+/**
+ * Calculates the inverse-document-frequency vector of a given list of documents
+ * 
+ * @param documents - list of documents
+ * @returns the idf vector
+ */
 function idf(documents: Vector[]): Vector {
+
     
     const n = documents.length;
 
@@ -88,6 +136,13 @@ function idf(documents: Vector[]): Vector {
 
 }
 
+/**
+ * Calculates the tf-idf vector of a particular document within a set of documents
+ * 
+ * @param tf_v - the term frequency vector of the document
+ * @param idf_v - the inverse document frequency vector of the list of documents
+ * @returns the tf-idf vector
+ */
 function tf_idf(tf_v: Vector, idf_v: Vector): Vector {
     
     let num_words = tf_v.get_length();
@@ -102,6 +157,12 @@ function tf_idf(tf_v: Vector, idf_v: Vector): Vector {
 
 }
 
+/**
+ * Returns the unit vector of a given vector
+ * 
+ * @param v - the vector
+ * @returns the respective unit vector
+ */
 function get_unit_vector(v: Vector): Vector {
     
     const size = v.get_length();
@@ -117,7 +178,15 @@ function get_unit_vector(v: Vector): Vector {
 
 }
 
+/**
+ * Calculates the dot product of two vectors
+ * 
+ * @param v1 - the first vector
+ * @param v2 - the second vector
+ * @returns the dot product
+ */
 function dot_product(v1: Vector, v2: Vector): number {
+    
     let prod = 0;
 
     for (let i = 0; i < v1.get_length(); i++) {
@@ -128,12 +197,26 @@ function dot_product(v1: Vector, v2: Vector): number {
 
  }
 
- function cosine_similarity(v1: Vector, v2: Vector): number {
+/**
+ * Calculates the cosine similarity of two vectors
+ * 
+ * @param v1 - the first vector
+ * @param v2 - the second vector
+ * @returns the cosine similarity
+ */ 
+function cosine_similarity(v1: Vector, v2: Vector): number {
 
     return dot_product(get_unit_vector(v1), get_unit_vector(v2));
 
  }
 
+ /**
+ * Returns the cosine similarity of documents and a query based on tf-idf weighting 
+ * 
+ * @param corpus - list of the documents
+ * @param query - the search query
+ * @returns the cosine similarity of each of the documents to the query
+ */
  function main(corpus: string[], query: string): number[] {
 
     let document_vectors: Vector[] = [];
