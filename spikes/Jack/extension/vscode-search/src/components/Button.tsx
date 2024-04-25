f84f4c1e4a40526ab500e45046247ba16cc71274
jackwigney <jack.wigney@gmail.com>
2024-04-25 17:57:46 +1000

    Jack Boolean spike commit

---
+++ spikes/Jack/extension/vscode-search/src/components/Button.tsx
@@ -0,0 +1,12 @@
+
+/* eslint-disable @typescript-eslint/naming-convention */
+import { EXTENSION_CONSTANT } from 'constant';
+function Button(){
+    return (
+        <button id={EXTENSION_CONSTANT.ELEMENT_IDS.TRIGGER_MESSAGE_BUTTON}>
+            Click to show message
+        </button>
+    );
+}
+
+export default Button;
---
+++ spikes/Jack/extension/vscode-search/src/components/LeftPanel.tsx
@@ -0,0 +1,20 @@
+
+/* eslint-disable @typescript-eslint/naming-convention */
+import Button from 'components/Button';
+
+interface LeftPanelProp {
+    message: string
+}
+
+function LeftPanel({
+    message
+}: LeftPanelProp){
+    return (
+        <div className='panel-wrapper'>
+            <span className='panel-info'>{message}</span>
+           <Button></Button>
+        </div>
+    );
+}
+
+export default LeftPanel;
